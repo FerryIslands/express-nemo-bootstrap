c3ea749e3a8af24d98f794b4351acd72576e164f
Kristoffer Ahl <git@77dynamite.com>
2018-06-07 14:03:34 +0200

    - Added logger and spec.

---
+++ logger.js
@@ -0,0 +1,78 @@
+const moment = require('moment')
+const extend = require('deep-extend')
+
+const loggingDisabled = () => {
+  return '' + process.env.LOGGING_DISABLED === 'true'
+}
+
+const getDefaultStructure = () => {
+  return {
+    timestamp: moment().format(),
+    level: 'trace',
+    message: 'Missing message!',
+    context: {
+      origin: {
+        name: 'express-nemo-bootstrap',
+        logger: 'logger.js'
+      }
+    }
+  }
+}
+
+const log = (data, level, context) => {
+  let structureMessage
+  const defaultStructure = getDefaultStructure()
+
+  if (typeof data !== 'object') {
+    structureMessage = extend({}, defaultStructure, {
+      message: data,
+      level: level,
+      context: context
+    })
+  } else {
+    const extendedData = extend({}, data, {
+      timestamp: defaultStructure.timestamp,
+      level: level,
+      context: context
+    })
+    structureMessage = extend({}, defaultStructure, extendedData)
+  }
+
+  if (!loggingDisabled()) {
+    let msg = JSON.stringify(structureMessage)
+    switch (level) {
+      case 'debug':
+      case 'info':
+      case 'warn':
+        console.log(msg)
+        break
+      case 'error':
+        console.error(msg)
+        break
+    }
+  }
+}
+
+class Logger {
+  constructor () {
+    this.context = {}
+  }
+
+  debug (data) {
+    log(data, 'debug', this.context)
+  }
+
+  info (data) {
+    log(data, 'info', this.context)
+  }
+
+  warn (data) {
+    log(data, 'warn', this.context)
+  }
+
+  error (data) {
+    log(data, 'error', this.context)
+  }
+}
+
+module.exports = Logger
