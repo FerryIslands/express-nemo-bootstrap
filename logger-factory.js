f0abc74de266808e9d3a72f1c7418178e8f1d4f7
Giorgio Salmi <giorgio.salmi@stena.com>
2018-06-26 13:48:01 +0200

    - Add missing req.context in logger

--- logger-factory.js
+++ logger-factory.js
@@ -1,14 +1,23 @@
 const Logger = require('./logger')
+const extend = require('deep-extend')
 
 module.exports = options => {
   const createLogger = (req, res) => {
-    return new Logger({
-      context: {
-        origin: {
-          name: options.application
+    return new Logger(
+      extend(
+        {},
+        {
+          context: {
+            origin: {
+              name: options.application
+            }
+          }
+        },
+        {
+          context: req.context
         }
-      }
-    })
+      )
+    )
   }
 
   return createLogger
