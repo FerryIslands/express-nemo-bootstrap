b3b48f8fd90ad66ffe7512e427eb909798e0791d
Sohan Fernando <sohan@graceful.nu>
2020-03-12 15:29:24 +0100

    added more graceful shutdown handling

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const defaultOptions = {
   basePath: '/'
 }
 
-const captureException = (error) => {
+const captureException = error => {
   if (process.env.SENTRY_DSN) {
     Sentry.captureException(error)
   }
 }
+
+let connections = []
 
 const expressNemo = options => {
   options = { ...defaultOptions, ...options }
@@ -47,9 +49,33 @@ const expressNemo = options => {
         logger.info(`Server is now running on port ${PORT}`)
       )
 
-      process.on('SIGTERM', () => {
-        server.close(() => logger.info(`Shutting down server...`))
+      server.on('connection', connection => {
+        connections.push(connection)
+        connection.on('close', () => {
+          connections = connections.filter(curr => curr !== connection)
+        })
       })
+
+      function shutDown () {
+        console.log('Received kill signal, shutting down gracefully')
+        server.close(() => {
+          console.log('Closed out remaining connections')
+          process.exit(0)
+        })
+
+        setTimeout(() => {
+          console.error(
+            'Could not close connections in time, forcefully shutting down'
+          )
+          process.exit(1)
+        }, 9900)
+
+        connections.forEach(curr => curr.end())
+        setTimeout(() => connections.forEach(curr => curr.destroy()), 5000)
+      }
+
+      process.on('SIGTERM', shutDown)
+      process.on('SIGINT', shutDown)
     }
   }
 }
